d574095c34ceb45c90b44352261939a5e2fc9133
Zeid Kootbally <zeidk@umd.edu>
2026-04-02 14:29:31 -0400

    updated single threaded demo code

--- src/executor_demo/executor_demo/single_threaded_demo_node.py
+++ src/executor_demo/executor_demo/single_threaded_demo_node.py
@@ -7,11 +7,10 @@ from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 class SingleThreadedDemoNode(Node):
     def __init__(self):
         super().__init__("single_threaded_demo")
-        self._group = MutuallyExclusiveCallbackGroup()
 
-        self.create_timer(0.5, self._cb1, callback_group=self._group)
-        self.create_timer(0.5, self._cb2, callback_group=self._group)
-        self.create_timer(0.25, self._cb3, callback_group=self._group)
+        self.create_timer(0.5, self._cb1)
+        self.create_timer(0.5, self._cb2)
+        self.create_timer(0.25, self._cb3)
 
     def _cb1(self):
         self.get_logger().info("cb1 START")
