a88f6ec10069a1162b2226800a15c28efa7d347e
kdeguzma <kdeguzma@umd.edu>
2026-04-21 20:05:45 -0400

    Added all initial files

--- src/gp2/group1_gp2/setup.py
+++ src/gp2/group1_gp2/setup.py
@@ -1,29 +1,32 @@
 from setuptools import find_packages, setup
 
-package_name = 'group1_gp2'
+package_name = "group1_gp2"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/launch", ["launch/gp2.launch.py"]),
+        ("share/" + package_name + "/config", ["config/goals.yaml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='kdeguzma',
-    maintainer_email='kdeguzma@umd.edu',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer="kdeguzma",
+    maintainer_email="kdeguzma@umd.edu",
+    description="TODO: Package description",
+    license="TODO: License declaration",
     extras_require={
-        'test': [
-            'pytest',
+        "test": [
+            "pytest",
         ],
     },
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
+            "navigate_to_goal_server = group1_gp2.scripts.main_navigate_to_goal_server:main",
+            "navigate_to_goal_client = group1_gp2.scripts.main_navigate_to_goal_client:main",
         ],
     },
 )
